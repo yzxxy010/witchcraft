0c6663a5a0010902658a78ac677e10b71d7d3a40
曜 星 <yzxxy2021@163.com>
2024-12-15 23:36:24 +0800

    去除bug多发地自动更新

--- code/window/AutoUpdate.cs
+++ code/window/AutoUpdate.cs
@@ -30,7 +30,6 @@ namespace VideoCopilot.code.window
 
         public static async void Init()
         {
-            DeleteFile();
             parentPath = Directory.GetParent(VideoCopilotClass.modDeclare.FolderPath).FullName;
             window = WindowCreator.CreateEmptyWindow("AutoUpdateWindow", "AutoUpdateWindow");
             var scrollView =
@@ -116,7 +115,8 @@ namespace VideoCopilot.code.window
                                  // $"当前mod已经有了新版本\n如果你的设备可以\n" +
                                  // $"<b><color=#ff0000>直接连接GitHub下载</color></b>\n" +
                                  $"当前mod已经有了新版本" +
-                                 $"请点击下方的更新";
+                                 $"如若更新,请前往q群:838697221\n" +
+                                 $"GitHub项目(国内无法访问):\nhttps://github.com/yzxxy010/witchcraft";
             // $"\n<b><color=#ff0000>否则</color></b>请自行更新,此页面可以关闭";
             textComponent.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             textComponent.fontSize = 50;
@@ -151,7 +151,7 @@ namespace VideoCopilot.code.window
             textObject.transform.SetParent(buttonObject.transform);
             Text textComponent = textObject.AddComponent<Text>();
             // textComponent.text = "<b><color=#ffffff>GitHub一键下载</color></b>";
-            textComponent.text = "<b><color=#ffffff>一键下载</color></b>";
+            textComponent.text = "<b><color=#ffffff>前往GitHub下载</color></b>";
             textComponent.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             textComponent.fontSize = 40;
             textComponent.alignment = TextAnchor.MiddleCenter;
@@ -163,167 +163,7 @@ namespace VideoCopilot.code.window
             textRectTransform.localPosition = Vector3.zero;
 
             // 添加按钮点击事件
-            buttonComponent.onClick.AddListener(() =>
-            {
-                string downloadUrl =
-                    "https://gh.tryxd.cn/https://github.com/yzxxy010/witchcraft/archive/refs/heads/master.zip";
-                string savePath = Path.Combine(Directory.GetParent(VideoCopilotClass.modDeclare.FolderPath).FullName,
-                    "witchcraft_latest.zip");
-                // 开始下载文件
-                content.AddComponent<AutoUpdate>().StartCoroutine(DownloadFileCoroutine(downloadUrl, savePath));
-            });
-        }
-
-        private static IEnumerator DownloadFileCoroutine(string downloadUrl, string savePath)
-        {
-            // 创建异步任务并使用协程等待
-            Task downloadTask = DownloadFileAsync(downloadUrl, savePath);
-
-            GameObject buttonObject = GameObject.Find("DownloadButton");
-            if (buttonObject != null)
-            {
-                Destroy(buttonObject);
-            }
-
-            var closeButton = GameObject.Find("CloseBackgound");
-            if (closeButton != null)
-            {
-                Destroy(closeButton);
-            }
-
-            while (!downloadTask.IsCompleted)
-            {
-                yield return null;
-            }
-
-            if (downloadTask.Exception != null)
-            {
-                // 处理下载失败的情况
-                Debug.LogError("下载失败: " + downloadTask.Exception.InnerException.Message);
-                textComponent.text = "下载失败，请重试。";
-            }
-            else
-            {
-                ExtractZipFileToRoot(Path.Combine(parentPath, "witchcraft_latest.zip"), parentPath);
-                DeleteFile();
-                DeleteFolder(VideoCopilotClass.modDeclare.FolderPath);
-                textComponent.text = "下载完成，请重启游戏。";
-            }
-        }
-
-        private static async Task DownloadFileAsync(string downloadUrl, string savePath)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    // 设置GitHub API请求的User-Agent头部，GitHub要求所有API请求必须包含这个头部
-                    client.DefaultRequestHeaders.Add("User-Agent", "Unity-Downloader");
-
-                    HttpResponseMessage response =
-                        await client.GetAsync(downloadUrl, HttpCompletionOption.ResponseHeadersRead);
-                    response.EnsureSuccessStatusCode();
-
-                    long totalBytes = response.Content.Headers.ContentLength ?? 0;
-                    long downloadedBytes = 0;
-
-                    // 创建一个流来接收数据并保存为文件
-                    using (Stream contentStream = await response.Content.ReadAsStreamAsync(),
-                           fileStream = new FileStream(savePath, FileMode.Create, FileAccess.Write, FileShare.None))
-                    {
-                        byte[] buffer = new byte[1024 * 1024];
-                        int bytesRead;
-                        while ((bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-                        {
-                            await fileStream.WriteAsync(buffer, 0, bytesRead);
-                            downloadedBytes += bytesRead;
-
-                            // 更新下载进度
-                            // long progress = (downloadedBytes / totalBytes)*100;
-                            textComponent.text = $"正在下载\n<color=#373b31>下载较慢请稍等</color>";
-                        }
-                    }
-
-                    Debug.Log("下载完成！");
-                }
-                catch (Exception ex)
-                {
-                    Debug.LogError("下载文件时出错: " + ex.Message);
-                    textComponent.text = "下载失败，请重试。";
-                }
-            }
-        }
-
-        private static void ExtractZipFileToRoot(string zipFilePath, string destinationDirectory)
-        {
-            try
-            {
-                if (!File.Exists(zipFilePath))
-                {
-                    Debug.LogError("ZIP 文件不存在: " + zipFilePath);
-                    return;
-                }
-
-                // 确保目标目录存在
-                if (!Directory.Exists(destinationDirectory))
-                {
-                    Directory.CreateDirectory(destinationDirectory);
-                }
-
-                // 解压整个 ZIP 文件到目标目录
-                ZipFile.ExtractToDirectory(zipFilePath, destinationDirectory);
-
-                Debug.Log("文件解压完成");
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError("解压文件时出错: " + ex.Message);
-            }
-        }
-
-
-        public static void DeleteFolder(string folderPath)
-        {
-            try
-            {
-                parentPath = Directory.GetParent(folderPath).FullName;
-
-                Directory.Delete(folderPath, true);
-                Debug.Log($"成功删除文件夹: {folderPath}");
-
-                Debug.Log($"上一级目录: {parentPath}");
-            }
-            catch (IOException ioEx)
-            {
-                Debug.LogError($"删除文件夹时发生IO错误: {ioEx.Message}");
-            }
-            catch (System.Exception ex)
-            {
-                Debug.LogError($"删除文件夹时发生错误: {ex.Message}");
-            }
-        }
-
-        public static void DeleteFile()
-        {
-            try
-            {
-                parentPath = Directory.GetParent(VideoCopilotClass.modDeclare.FolderPath).FullName;
-                Debug.Log(parentPath);
-                if (!File.Exists(Path.Combine(parentPath, "witchcraft_latest.zip")))
-                {
-                    return;
-                }
-
-                File.Delete(Path.Combine(parentPath, "witchcraft_latest.zip"));
-            }
-            catch (IOException ioEx)
-            {
-                Debug.LogError($"删除文件时发生IO错误: {ioEx.Message}");
-            }
-            catch (System.Exception ex)
-            {
-                Debug.LogError($"删除文件时发生错误: {ex.Message}");
-            }
+            buttonComponent.onClick.AddListener(() => { Application.OpenURL("https://325477.xyz");});
         }
     }
 }
